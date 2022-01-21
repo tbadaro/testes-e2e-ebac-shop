4ea99b50564bc424ba8ad1da21367fc17a4c07ed
Tadeu Badaró <t.badaro@gmail.com>
2022-01-21 03:35:46 -0300

    duas versões do teste end to end criado; adicionados custom commands, page objects de endereço e arquivos de dados.

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,4 +1,9 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page_objects/endereco.page';
+const dadosEndereco = require('../fixtures/enderecos.json')
+var faker = require('faker');
+faker.locale = 'pt_BR';
+
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
@@ -10,12 +15,84 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('produtos/')
+    });
+    afterEach(() => {
+        cy.screenshot()
     });
 
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {       
+        let nomeFaker = faker.name.firstName()
+        let sobrenomeFaker = faker.name.lastName()
+        let empresaFaker = faker.company.companyName()
+        let emailFaker = faker.internet.email(nomeFaker)
+        let telFaker = faker.phone.phoneNumber()
+        let ruaFaker = faker.address.streetName()
+        let numeroFaker = faker.datatype.number()
+        let cidadeFaker = faker.address.city()
+        let estadoFaker = faker.address.state()
+        let cepFaker = faker.address.zipCode()
+        
+        //adicionando 4 produtos ao carrinho de páginas diferentes
+        cy.addProduto('Argus All-Weather Tank', 'M', 'Gray', 1)
+        cy.get('.tbay-woocommerce-breadcrumb > :nth-child(2) > a').click()
+        cy.get(':nth-child(2) > .page-numbers').click()
+        cy.addProduto('Bruno Compete Hoodie', 'L', 'Blue', 1)
+        cy.get('.tbay-woocommerce-breadcrumb > :nth-child(2) > a').click()
+        cy.get(':nth-child(3) > .page-numbers').click()
+        cy.addProduto('Cronus Yoga Pant', '33', 'Black', 1)
+        cy.get('.tbay-woocommerce-breadcrumb > :nth-child(2) > a').click()
+        cy.get('ul.page-numbers > :nth-child(4) > .page-numbers').click()
+        cy.addProduto('Hawkeye Yoga Short', '36', 'Gray', 1)
+        //checkout preenchendo dados com faker
+        cy.get('.woocommerce-message > .button').click()
+        cy.get('.checkout-button').click()
+        EnderecoPage.editarEnderecoFaturamento(
+            nomeFaker, 
+            sobrenomeFaker, 
+            empresaFaker, 
+            'Brasil', 
+            ruaFaker, 
+            numeroFaker, 
+            cidadeFaker, 
+            estadoFaker, 
+            cepFaker, 
+            telFaker, 
+            emailFaker)
+            cy.get('#terms').check()
+            cy.get('#place_order').click()
+            cy.get('.woocommerce-notice').should('contain','Obrigado. Seu pedido foi recebido.')
     });
 
-
+    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta - usando arquivo de dados', () => {               
+        //adicionando 4 produtos ao carrinho de páginas diferentes
+        cy.addProduto('Argus All-Weather Tank', 'M', 'Gray', 1)
+        cy.get('.tbay-woocommerce-breadcrumb > :nth-child(2) > a').click()
+        cy.get(':nth-child(2) > .page-numbers').click()
+        cy.addProduto('Bruno Compete Hoodie', 'L', 'Blue', 1)
+        cy.get('.tbay-woocommerce-breadcrumb > :nth-child(2) > a').click()
+        cy.get(':nth-child(3) > .page-numbers').click()
+        cy.addProduto('Cronus Yoga Pant', '33', 'Black', 1)
+        cy.get('.tbay-woocommerce-breadcrumb > :nth-child(2) > a').click()
+        cy.get('ul.page-numbers > :nth-child(4) > .page-numbers').click()
+        cy.addProduto('Hawkeye Yoga Short', '36', 'Gray', 1)
+        //checkout preenchendo dados com arquivo de dados
+        cy.get('.woocommerce-message > .button').click()
+        cy.get('.checkout-button').click()
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email)
+            cy.get('#terms').check()
+            cy.get('#place_order').click()
+            cy.get('.woocommerce-notice').should('contain','Obrigado. Seu pedido foi recebido.')
+    });
 })
